abdf6425fd0b284e963eb67124c4a7618703de8f
Profe Matias Prestes <profematiasprestes@gmail.com>
2026-09-02 00:03:36 -0300

    feat: create BenefitBullet component to display feature benefits with animation

--- src/components/Benefits/BenefitBullet.tsx
+++ src/components/Benefits/BenefitBullet.tsx
@@ -9,7 +9,7 @@ const BenefitBullet: React.FC<IBenefitBullet> = ({ title, description, icon }: I
             className="flex flex-col items-center mt-8 gap-3 lg:gap-5 lg:flex-row lg:items-start"
             variants={childVariants}
         >
-            <div className="flex justify-center mx-auto lg:mx-0 flex-shrink-0 mt-3 w-fit">
+            <div className="flex justify-center mx-auto lg:mx-0 shrink-0 mt-3 w-fit">
                 {icon}
             </div>
             <div>
